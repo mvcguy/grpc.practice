a0f7660170f9e857d4ee19e9477534cb29f86dd2
Shahid Ali <me.shahidali@hotmail.com>
2020-11-07 00:57:13 +0100

    Add dice game app (#3)
    * added dice game
    
    * shutdown the stream gracefully

---
+++ WebService/Services/PlayDiceSrv/PlayDiceService.cs
@@ -0,0 +1,42 @@
+﻿using DiceGame;
+using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace WebService.Services.PlayDiceSrv
+{
+    public class PlayDiceService : DiceGameService.DiceGameServiceBase
+    {
+
+        public override async Task Start(IAsyncStreamReader<GameRequest> requestStream,
+            IServerStreamWriter<GameResponse> responseStream, ServerCallContext context)
+        {
+            await foreach (var item in requestStream.ReadAllAsync())
+            {
+                if (item.EndGame) break;
+                //
+                // client has played its dice
+                //
+                var serverResult = await RollDice();
+                await responseStream.WriteAsync(new GameResponse { ServerResult = $"Server result: {serverResult}" });
+            }
+        }
+
+        public override async Task<DiceResult> RollDice(EmptyRequest request, ServerCallContext context)
+        {
+            var result = await RollDice();
+            return new DiceResult { Result = result };
+        }
+
+        private Task<int> RollDice()
+        {
+            var task1 = Task.Run(async () =>
+            {
+                await Task.Delay(2000);
+                var rnd = new Random(Math.Abs(Guid.NewGuid().GetHashCode()));
+                return rnd.Next(1, 7);
+            });
+            return task1;
+        }
+    }
+}
--- WebService/Startup.cs
+++ WebService/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebService.Services.PlayDiceSrv;
 
 namespace GrpcService1
 {
@@ -35,7 +36,7 @@ namespace GrpcService1
             {
                 endpoints.MapGrpcService<GreeterService>();
                 endpoints.MapGrpcService<CustomersMaintenanceService>();
-
+                endpoints.MapGrpcService<PlayDiceService>();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
