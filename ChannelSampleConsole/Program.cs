5aefb751c82ac905f49f77c6d5df3c271bc3cbfa
MvcGuy <me.shahidali@hotmail.com>
2020-11-08 23:00:26 +0100

    origin/main test

--- ChannelSampleConsole/Program.cs
+++ ChannelSampleConsole/Program.cs
@@ -8,7 +8,7 @@ namespace ChannelSampleConsole
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Channels demo");
             try
             {
                 await UnboundedChannelSample.Run();
