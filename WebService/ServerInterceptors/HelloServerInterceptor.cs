fd91332a8ae46a067e5ae9cb39849e2d7e757ff9
MvcGuy <me.shahidali@hotmail.com>
2020-11-10 00:54:21 +0100

    added implementation for grpc service and a demo interceptor

---
+++ WebService/ServerInterceptors/HelloServerInterceptor.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Interceptors;
+using Microsoft.Extensions.Logging;
+
+namespace WebService.ServerInterceptors
+{
+    /// <summary>
+    /// taken from grpc-dotnet github samples
+    /// </summary>
+    public class HelloServerInterceptor : Interceptor
+    {
+        private readonly ILogger<HelloServerInterceptor> logger;
+
+        public HelloServerInterceptor(ILogger<HelloServerInterceptor> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, 
+            ServerCallContext context, UnaryServerMethod<TRequest, TResponse> continuation)
+        {
+            LogCall<TRequest, TResponse>(MethodType.Unary, context);
+            return base.UnaryServerHandler(request, context, continuation);
+        }
+
+        public override Task<TResponse> ClientStreamingServerHandler<TRequest, TResponse>(
+           IAsyncStreamReader<TRequest> requestStream,
+           ServerCallContext context,
+           ClientStreamingServerMethod<TRequest, TResponse> continuation)
+        {
+            LogCall<TRequest, TResponse>(MethodType.ClientStreaming, context);
+            return base.ClientStreamingServerHandler(requestStream, context, continuation);
+        }
+
+        public override Task ServerStreamingServerHandler<TRequest, TResponse>(
+            TRequest request,
+            IServerStreamWriter<TResponse> responseStream,
+            ServerCallContext context,
+            ServerStreamingServerMethod<TRequest, TResponse> continuation)
+        {
+            LogCall<TRequest, TResponse>(MethodType.ServerStreaming, context);
+            return base.ServerStreamingServerHandler(request, responseStream, context, continuation);
+        }
+
+        public override Task DuplexStreamingServerHandler<TRequest, TResponse>(
+            IAsyncStreamReader<TRequest> requestStream,
+            IServerStreamWriter<TResponse> responseStream,
+            ServerCallContext context,
+            DuplexStreamingServerMethod<TRequest, TResponse> continuation)
+        {
+            LogCall<TRequest, TResponse>(MethodType.DuplexStreaming, context);
+            return base.DuplexStreamingServerHandler(requestStream, responseStream, context, continuation);
+        }
+
+        private void LogCall<TRequest, TResponse>(MethodType methodType, ServerCallContext context)
+            where TRequest : class
+            where TResponse : class
+        {
+            logger.LogWarning($"Starting call. Type: {methodType}. Request: {typeof(TRequest)}. Response: {typeof(TResponse)}");
+            WriteMetadata(context.RequestHeaders, "caller-user");
+            WriteMetadata(context.RequestHeaders, "caller-machine");
+            WriteMetadata(context.RequestHeaders, "caller-os");
+
+            void WriteMetadata(Metadata headers, string key)
+            {
+                var headerValue = headers.SingleOrDefault(h => h.Key == key)?.Value;
+                logger.LogWarning($"{key}: {headerValue ?? "(unknown)"}");
+            }
+        }
+    }
+}
---
+++ WebService/Services/IntercepterSrv/InterceptorDemoService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using HelloIntercepter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.Services.IntercepterSrv
+{
+    public class InterceptorDemoService : HelloIntercepterService.HelloIntercepterServiceBase
+    {
+        public override async Task<SilentReply> SendToUniverse(Message request, ServerCallContext context)
+        {
+            await Task.Delay(100);
+            var message = $"Your message is received. ID: {request.Id}, Contents: {request.Contents}";
+            return new SilentReply { Contents = message, Id = Guid.NewGuid().ToString() };
+        }
+
+
+    }
+}
