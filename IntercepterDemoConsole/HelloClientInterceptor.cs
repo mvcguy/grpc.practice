61a30e84162888df8a46149552bec0f655616bcf
MvcGuy <me.shahidali@hotmail.com>
2020-11-10 22:23:29 +0100

    - Added intercepter for client - Added demo client

---
+++ IntercepterDemoConsole/HelloClientInterceptor.cs
@@ -0,0 +1,69 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+using Microsoft.DotNet.PlatformAbstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntercepterDemoConsole
+{
+    public class HelloClientInterceptor : Interceptor
+    {       
+        public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(TRequest request,
+            ClientInterceptorContext<TRequest, TResponse> context,
+            AsyncUnaryCallContinuation<TRequest, TResponse> continuation)
+        {
+            var headers = context.Options.Headers;
+            if (headers == null)
+            {
+                headers = new Metadata()
+                {
+                    new Metadata.Entry("caller-user", Environment.UserName),
+                    new Metadata.Entry("caller-machine", Environment.MachineName),
+                    new Metadata.Entry("caller-os",
+                    $"OS: {RuntimeEnvironment.OperatingSystem}, " +
+                    $"Platform: {RuntimeEnvironment.OperatingSystemPlatform}, " +
+                    $"Version: {RuntimeEnvironment.OperatingSystemVersion}"),
+                };
+                var options = context.Options.WithHeaders(headers);
+                context = new ClientInterceptorContext<TRequest, TResponse>(context.Method, context.Host, options);
+            }
+            LogCall(context.Method);
+            return base.AsyncUnaryCall(request, context, continuation);
+        }
+
+        public override AsyncDuplexStreamingCall<TRequest, TResponse>
+            AsyncDuplexStreamingCall<TRequest, TResponse>(ClientInterceptorContext<TRequest, TResponse> context,
+            AsyncDuplexStreamingCallContinuation<TRequest, TResponse> continuation)
+        {
+            return base.AsyncDuplexStreamingCall(context, continuation);
+        }
+
+        public override AsyncClientStreamingCall<TRequest, TResponse>
+            AsyncClientStreamingCall<TRequest, TResponse>(ClientInterceptorContext<TRequest, TResponse> context,
+            AsyncClientStreamingCallContinuation<TRequest, TResponse> continuation)
+        {
+            return base.AsyncClientStreamingCall(context, continuation);
+        }
+
+        public override AsyncServerStreamingCall<TResponse> AsyncServerStreamingCall<TRequest, TResponse>(TRequest request,
+            ClientInterceptorContext<TRequest, TResponse> context,
+            AsyncServerStreamingCallContinuation<TRequest, TResponse> continuation)
+        {
+            return base.AsyncServerStreamingCall(request, context, continuation);
+        }
+
+        private void LogCall<TRequest, TResponse>(Method<TRequest, TResponse> method)
+           where TRequest : class
+           where TResponse : class
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"CLIENT INTERCEPTOR - Handling a GRPC call. Request: {typeof(TRequest)}, " +
+                $"Response: {typeof(TResponse)}, Method: {method.Name}");
+            Console.WriteLine(Environment.NewLine);
+            Console.ResetColor();
+        }
+
+
+    }
+}
--- IntercepterDemoConsole/Program.cs
+++ IntercepterDemoConsole/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+using Grpc.Net.Client;
+using HelloIntercepter;
+using System;
+using System.Threading.Tasks;
+using static HelloIntercepter.HelloIntercepterService;
 
 namespace IntercepterDemoConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            await InterceptorDemoClient.Run();
+
+            Console.WriteLine("Press any key to exist");
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class InterceptorDemoClient
+    {
+        static readonly GrpcChannel Channel = GrpcChannel.ForAddress("https://localhost:5001");
+        static readonly CallInvoker invoker = Channel.Intercept(new HelloClientInterceptor());
+        static readonly HelloIntercepterServiceClient Client = new HelloIntercepterServiceClient(invoker);
+
+        public static async Task Run()
+        {
+            var reply = await Client.SendToUniverseAsync(new Message
+            {
+                Id = "sha-171",
+                Contents = "Alhamdulillah - All praise to Allah, " +
+                "THE CREATOR of everything that we see, and that we dont see"
+            });
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Server Reply - ID: {reply.Id}, Contents: {reply.Contents}");
+            Console.ResetColor();
         }
     }
 }
