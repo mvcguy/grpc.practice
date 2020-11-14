def59262ab62fd564f65893aebe8b3bdd7734142
MvcGuy <me.shahidali@hotmail.com>
2020-11-14 02:09:39 +0100

    add unit test project

---
+++ UnitTests/CommonLibraryTests/UtilityTests.cs
@@ -0,0 +1,21 @@
+using CommonLibrary;
+using NUnit.Framework;
+
+namespace UnitTests.CommonLibraryTests
+{
+    public class UtilityTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test(Description ="Verify that the method returns a string of given length", TestOf =typeof(Utilities))]
+        
+        public void RandomString01()
+        {
+            var result = Utilities.RandomString(8);
+            Assert.AreEqual(8, result.Length);
+        }
+    }
+}
