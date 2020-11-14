be2464afde4f2abb009484303a8a8ce23c313ad3
MvcGuy <me.shahidali@hotmail.com>
2020-11-14 01:39:07 +0100

    added demo for duplex stream interceptor

--- IntercepterDemoConsole/App/InterceptorDemoClient.cs
+++ IntercepterDemoConsole/App/InterceptorDemoClient.cs
@@ -6,6 +6,7 @@ using Grpc.Core.Interceptors;
 using System;
 using System.Threading.Tasks;
 using static HelloIntercepter.HelloIntercepterService;
+using CommonLibrary;
 
 namespace IntercepterDemoConsole.App
 {
@@ -21,6 +22,7 @@ namespace IntercepterDemoConsole.App
             await DemoUnaryCall();
             await DemoClientStream();
             await DemoServerStream();
+            await DemoDuplexStream();
             Console.ResetColor();
         }
 
@@ -38,8 +40,8 @@ namespace IntercepterDemoConsole.App
         private static async Task DemoClientStream()
         {
             var call = Client.UploadToUniverse();
-            await call.RequestStream.WriteAsync(new Tales { Id="SHA171", Contents="WE LOVE PROPHET MUHAMMAD PEACE OF ALLAH BE UPON HIM <3 <3 <3" });
-            await call.RequestStream.WriteAsync(new Tales { Id="SHA171", Contents="We love Him because he brought us to the light from clear darkness." });
+            await call.RequestStream.WriteAsync(new Tales { Id = "SHA171", Contents = "WE LOVE PROPHET MUHAMMAD PEACE OF ALLAH BE UPON HIM <3 <3 <3" });
+            await call.RequestStream.WriteAsync(new Tales { Id = "SHA171", Contents = "We love Him because he brought the whole of humanity to the light from clear darkness." });
 
             await call.RequestStream.CompleteAsync();
             var reply = await call.ResponseAsync;
@@ -56,8 +58,44 @@ namespace IntercepterDemoConsole.App
 
         private static async Task DemoDuplexStream()
         {
+            var method = Client.CommunicateToUniverse();
+            var responseStrm = method.ResponseStream;
+            var requestStrm = method.RequestStream;
+            var count = 0;
+
+            await SendRandomMessage(requestStrm);
+
+            await foreach (var item in responseStrm.ReadAllAsync())
+            {
+                WriteConsole(item);
+                await SendRandomMessage(requestStrm);
+                if (count++ == 10) break;
+            }
+
+            await requestStrm.CompleteAsync();
+
+            //
+            // read any remaining messages
+            //
+            await foreach (var item in responseStrm.ReadAllAsync())
+            {
+                WriteConsole(item);
+            }
 
         }
+
+        private static async Task SendRandomMessage(IClientStreamWriter<Message> requestStrm)
+        {
+            var randomMessage = new Message
+            {
+                Id = Guid.NewGuid().GetHashCode().ToString(),
+                Contents = Utilities.RandomSentence()
+            };
+
+            await requestStrm.WriteAsync(randomMessage);
+            await Task.Delay(1000);
+        }
+
         private static void WriteConsole(Message reply)
         {
             Console.WriteLine($"Server Reply - ID: {reply.Id}, Contents: {reply.Contents}");
--- WebService/Services/IntercepterSrv/InterceptorDemoService.cs
+++ WebService/Services/IntercepterSrv/InterceptorDemoService.cs
@@ -1,5 +1,7 @@
-﻿using Grpc.Core;
+﻿using CommonLibrary;
+using Grpc.Core;
 using HelloIntercepter;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,13 @@ namespace WebService.Services.IntercepterSrv
 {
     public class InterceptorDemoService : HelloIntercepterService.HelloIntercepterServiceBase
     {
+        private readonly ILogger<InterceptorDemoService> logger;
+
+        public InterceptorDemoService(ILogger<InterceptorDemoService> logger)
+        {
+            this.logger = logger;
+        }
+
         public override async Task<SilentReply> SendToUniverse(Message request, ServerCallContext context)
         {
             await Task.Delay(100);
@@ -37,7 +46,35 @@ namespace WebService.Services.IntercepterSrv
                 Id = Guid.NewGuid().ToString(),
                 Contents = $"your inquiry is arrived. {Environment.NewLine} " +
                 $"\"ID: {request.Id} - {request.Contents}\""
-            });            
+            });
+        }
+
+        public override async Task CommunicateToUniverse(IAsyncStreamReader<Message> requestStream,
+            IServerStreamWriter<Message> responseStream, ServerCallContext context)
+        {
+            var count = 0;
+            await foreach (var item in requestStream.ReadAllAsync())
+            {
+                logger.LogInformation($"{item.Id}: {item.Contents}");
+
+                var randomMessage = new Message
+                {
+                    Id = Guid.NewGuid().GetHashCode().ToString(),
+                    Contents = Utilities.RandomSentence()
+                };
+
+                await responseStream.WriteAsync(randomMessage);
+                await Task.Delay(1000);
+                if (count++ == 10) break;
+            }
+
+            //
+            // read any remaining messages
+            //
+            await foreach (var item in requestStream.ReadAllAsync())
+            {
+                logger.LogInformation($"{item.Id}: {item.Contents}");
+            }
         }
 
     }
